744cfcc1c2f42350d2c58e8416e07999743a8170
Victor Weiss <bonjour@victor-weiss.fr>
2021-03-23 12:44:11 +0100

    [WIP] blog

--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -2,12 +2,17 @@
 
 namespace App\Controller;
 
+use App\Entity\Article;
+use App\Form\ArticleType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class BlogController extends AbstractController
 {
+    # --- Front
+
     /**
      * @Route("/blog", name="blog")
      */
@@ -25,6 +30,42 @@ class BlogController extends AbstractController
     {
         return $this->render('blog/article.html.twig', [
 
+        ]);
+    }
+
+
+    # --- Back
+
+    /**
+     * @Route("/mon-espace/articles", name="membre_blog_list")
+     */
+    public function membreBlogList()
+    {
+        return $this->render('blog/membre/list.html.twig', [
+
+        ]);
+    }
+
+    /**
+     * @Route("/mon-espace/articles/nouveau", name="membre_blog_create")
+     */
+    public function membreBlogCreate(Request $request)
+    {
+        $article = new Article();
+        $form = $this->createForm(ArticleType::class, $article);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($article);
+            $em->flush();
+
+            $this->addFlash('success', "L'article a bien été créé.");
+            return $this->redirectToRoute('membre_blog_list');
+        }
+
+        return $this->render('blog/membre/create.html.twig', [
+            'form' => $form->createView(),
         ]);
     }
 }
---
+++ src/Entity/Article.php
@@ -0,0 +1,87 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ArticleRepository;
+use DateTime;
+use DateTimeInterface;
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
+
+/**
+ * @ORM\Entity(repositoryClass=ArticleRepository::class)
+ */
+class Article
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     * @Assert\NotBlank
+     */
+    private $title;
+
+    /**
+     * @ORM\Column(type="text")
+     * @Assert\NotBlank
+     */
+    private $content;
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    private $createdAt;
+
+    public function __construct()
+    {
+        $this->createdAt = new DateTime();
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTitle(): ?string
+    {
+        return $this->title;
+    }
+
+    public function setTitle(string $title): self
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    public function getContent(): ?string
+    {
+        return $this->content;
+    }
+
+    public function setContent(string $content): self
+    {
+        $this->content = $content;
+
+        return $this;
+    }
+
+    public function getCreatedAt(): ?DateTimeInterface
+    {
+        return $this->createdAt;
+    }
+
+    public function setCreatedAt(DateTimeInterface $createdAt): self
+    {
+        $this->createdAt = $createdAt;
+
+        return $this;
+    }
+
+
+}
