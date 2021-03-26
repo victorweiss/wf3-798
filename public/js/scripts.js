b97cfb07b3d0cb05c3eb3760bc342c7a228931dd
Victor Weiss <bonjour@victor-weiss.fr>
2021-03-26 16:17:43 +0100

    Error pages + Chosen JS

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -4,3 +4,7 @@ $('form button[type="submit"]').click(function() {
     $(this).text('Chargement...').attr('disabled', true)
     $(this).closest('form').submit()
 })
+
+$(document).ready(function() {
+    $('select').chosen()
+})
